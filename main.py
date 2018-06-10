31991c2ca7da2c112a7fe03b75b6399bdd06733f
jack-pincombe <jackpincombe1@gmail.com>
2018-06-10 20:24:40 +0100

    Inital code to get prices on first page

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-import requests 
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+from urllib.error import HTTPError
+import re
 
-response = requests.get('https://httpbin.org/ip')
+url = "https://www.superbikefactory.co.uk/used-motorcycles-macclesfield-cheshire"
 
-print('Your IP is {0}'.format(response.json()['origin']))
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+
+    if html is None:
+        print("Shit has hit the fan")
+    else:
+        bsObj = bs(html.read(), "html.parser")
+        getAllPrices(bsObj)
+
+def getAllPrices(bsObj):
+    list = []
+
+    for tag in bsObj.findAll('span', {'class': 'price-is'}):
+        list.append(getBikePrice(tag))
+
+def getBikePrice(tag):
+    p = re.findall('(\S[1-9].*\d)', str(tag))[0]
+    print("The price of the bike is " + p)
+
+getTitle(url)
