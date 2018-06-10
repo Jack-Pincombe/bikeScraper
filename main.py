26e674cf5a5d4bfbfb9c0bb279ef7c9f80404042
jack-pincombe <jackpincombe1@gmail.com>
2018-06-10 23:01:57 +0100

    Attempting matching price and model

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from urllib.error import HTTPError
+from selenium import webdriver
 import re
 
 url = "https://www.superbikefactory.co.uk/search_page.php?term=ducati-macclesfield-cheshire&ccto=9999&sort=h"
+firefox_path = "/home/jack/Desktop/"
+
+driver = webdriver.Firefox(firefox_path)
+driver.get('https://www.superbikefactory.co.uk/used-motorcycles-macclesfield-cheshire')
+
+def siteNum(pageNo):
+    buttonPath = """/html/body/div[1]/div[2]/div[2]/div/div/div/div[4]/div/div/div/div/div[3]/ol/li["""+ str(pageNo) + """]/a"""
+    return(buttonPath)
 
 def getTitle(url):
     try:
@@ -24,8 +33,38 @@ def getAllPrices(bsObj):
 
     print(len(list))
 
+def getBikeMake(bsObj):
+    pass
+
+
+
 def getBikePrice(tag):
     price = re.findall('(\S[1-9].*\d)', str(tag))[0]
     print("The price of the bike is " + price)
 
-getTitle(url)
+def siteMove():
+    pageNo = 5
+    while pageNo != 3:
+        getTitle(driver.current_url)
+
+        driver.find_element_by_xpath(
+            siteNum(pageNo)).click()
+        pageNo -= 1
+
+
+    for i in range(10):
+        driver.find_element_by_xpath(
+            siteNum(pageNo)).click()
+        getTitle(driver.current_url)
+
+def setUp():
+    firefox_path = "/home/jack/Desktop/"
+
+    driver = webdriver.Firefox(firefox_path)
+    driver.get('https://www.superbikefactory.co.uk/used-motorcycles-macclesfield-cheshire')
+
+def __main__():
+    #setUp()
+    siteMove()
+
+__main__()
--- moveWeb.py
+++ moveWeb.py
@@ -1,33 +1,41 @@
-#!/jack/bin/env python
+import re
+from urllib.error import HTTPError
+from urllib.request import urlopen
 
-from selenium import webdriver
-
-pageNo = 5
-
-def siteNum(pageNo):
-    buttonPath = """/html/body/div[1]/div[2]/div[2]/div/div/div/div[4]/div/div/div/div/div[3]/ol/li["""+ str(pageNo) + """]/a"""
-    return(buttonPath)
-    #while i != 3:
-
-firefox_path = "/home/jack/Desktop/"
-
-driver = webdriver.Firefox(firefox_path)
-driver.get('https://www.superbikefactory.co.uk/used-motorcycles-macclesfield-cheshire')
-
-while pageNo != 3:
-    driver.find_element_by_xpath(
-        siteNum(pageNo)).click()
-
-    pageNo -= 1
-
-for i in range(10):
-    driver.find_element_by_xpath(
-        siteNum(pageNo)).click()
-    print(driver.current_url)
-
-print("Finished")
+from bs4 import BeautifulSoup as bs
 
 
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+
+    if html is None:
+        print("Shit has hit the fan")
+    else:
+        bsObj = bs(html.read(), "html.parser")
+        getAllPrices(bsObj)
+
+def getAllPrices(bsObj):
+    list = []
+    for container in bsObj.findAll('div', {'class': 'container'}):
+        price = getBikePrice(container.findAll('span', {'class': 'price-is'}))
+        text = getBikeMake(container.find('h3'))
 
 
+        print("The cost of " + str(getBikeMake(text)) + " is " + str(getBikePrice(price)))
 
+def getBikeMake(tag):
+    make = re.compile('\"View Vehicle Details\"(.*\S)/')
+
+    if make.match(str(tag)):
+        return make
+
+def getBikePrice(tag):
+    price = re.compile('(\S[1-9].*\d)')
+
+    if price.match(str(tag)):
+        return price
+
+getTitle('https://www.superbikefactory.co.uk/used-motorcycles-macclesfield-cheshire')
